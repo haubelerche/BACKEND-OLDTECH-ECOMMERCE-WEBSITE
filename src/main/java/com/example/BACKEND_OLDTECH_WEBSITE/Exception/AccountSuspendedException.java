8ae9182bfc5479063e78f39a926637e299b0a128
Admin <luonghau2909@gmail.com>
2025-05-22 02:05:56 +0700

    First commit

---
+++ src/main/java/com/example/BACKEND_OLDTECH_WEBSITE/Exception/AccountSuspendedException.java
@@ -0,0 +1,16 @@
+package com.example.BACKEND_OLDTECH_WEBSITE.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class AccountSuspendedException extends RuntimeException {
+
+    public AccountSuspendedException(String message) {
+        super(message);
+    }
+
+    public AccountSuspendedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
